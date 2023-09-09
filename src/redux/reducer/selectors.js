18980f552db8ffc971e24daa6e8f9de88dfaec98
nkhademi <nkhademi2731@gmail.com>
2023-09-09 23:46:57 +0330

    debug selector

--- src/redux/reducer/selectors.js
+++
@@ -1,6 +0,0 @@
-import {createSelector} from "reselect";
-
-/*----------------------- user --------------------------- */
-const selectUser = ({user}) => user;
-
-export const userSelector = createSelector(selectUser, (allSelectUser) => allSelectUser);
